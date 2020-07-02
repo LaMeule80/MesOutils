daad6825d8bba09c8b1cc910e0e86fccb54197b1
Jerôme MUSQUA <jmusqua@wiuz.fr>
2020-07-02 10:33:24 +0200

    Create ApiHelper.cs

---
+++ OutilsBlazor/ApiHelper.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace OutilsBlazor
+{
+    public abstract class ApiHelper
+    {
+        public abstract string ApiKey { get; }
+
+        public abstract string UrlBase { get;}
+
+        public abstract string Uri { get; }
+
+        public string Adresse => $"{UrlBase}/{Uri}";
+
+        public TData Get<TData>(string url, List<KeyValuePair<string, object>> param = null) where TData : class, new()
+        {
+            if (param == null)
+                param = new List<KeyValuePair<string, object>>();
+            param.Add(new KeyValuePair<string, object>("key", ApiKey));
+
+            param.ForEach(x => url += $"&{x.Key}={x.Value}");
+
+            TData data;
+
+            HttpWebRequest request = WebRequest.CreateHttp(url);
+            request.Method = WebRequestMethods.Http.Get;
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            {
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    using (StreamReader myStreamReader = new StreamReader(responseStream, Encoding.UTF8))
+                    {
+                        string responseJSON = myStreamReader.ReadToEnd();
+                        data = JsonConvert.DeserializeObject<TData>(responseJSON);
+                    }
+                }
+            }
+
+            return data;
+        }
+    }
+}
